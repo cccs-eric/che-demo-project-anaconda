f4fe8dfd67efbd50758f1e3696828a68753ab173
eric.ladouceur@cyber.gc.ca <eric.ladouceur@cyber.gc.ca>
2021-01-05 21:28:53 +0000

    Adding commands for conda

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+import pathlib 
+import numpy as np
+from whatlies.language import CountVectorLanguage, UniversalSentenceLanguage, BytePairLanguage, SentenceTFMLanguage
+from whatlies.transformers import Pca, Umap
+
 print('Eclipse Che rocks!')
